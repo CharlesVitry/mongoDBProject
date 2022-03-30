1230759cf9eeb38acc3c280859c6fab415e113db
Oka <45233151+CharlesVitry@users.noreply.github.com>
2022-03-30 11:17:12 +0200

    add Etablissement Test

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -3,6 +3,7 @@ package main;
 import com.mongodb.client.model.InsertOneModel;
 import dao.Dao;
 import dao.DaoFactory;
+import dao.EtablissementDao;
 import model.Adresse;
 import model.Etablissement;
 import model.Etudiant;
@@ -24,6 +25,7 @@ public class App<ad1>
         Dao<Adresse> adresseDao = DaoFactory.getAdresseDAO();
         Dao<Etudiant> etudiantDao = DaoFactory.getEtudiantDAO();
         Dao<Formation> formationDao = DaoFactory.getFormationDAO();
+        Dao<Etablissement> EtablissementDao = DaoFactory.getEtablissementDAO();
 
         Adresse ad1 = new Adresse(45, "Rue rabelaie", "Angers", 49000, "49", 49.3, 49.7);
         adresseDao.create(ad1);
@@ -37,7 +39,9 @@ public class App<ad1>
         etudiantDao.create(et1);
         System.out.println(etudiantDao.find(et1));
 
-        Etablissement eta1 = new Etablissement(1,)
+        Etablissement eta1 = new Etablissement(1,"sigle","JASA","+33(0)6 87 14 89","secondaire","privé","UCO",ad1,
+                new ArrayList<String>(),new ArrayList<String>(),new ArrayList<String>() );
+        EtablissementDao.create(eta1);
 
 
     }
